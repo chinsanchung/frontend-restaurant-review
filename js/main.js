4cd62299898aab45a413ee00cb7b1b93c2ebb7ad
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2018-07-21 23:43:01 +0900

    Fix main.js
    Change service worker checking code

--- js/main.js
+++ js/main.js
@@ -178,14 +178,8 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 }
 // change : service worker setting
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/js/serviceWorker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('../sw.js')
+    .then(function() { console.log("service worker registered in main.js"); })
 }
