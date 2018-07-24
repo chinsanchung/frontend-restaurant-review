663f3d076f0a97c9de62867fe3f01c450fcf3ca4
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2018-07-24 17:01:15 +0900

    Fix sw.js
    Fix commit error

--- sw.js
+++ sw.js
@@ -1,36 +1,4 @@
 /* change: This js file has service worker codes.  */
-
-self.addEventListener('install', function(ev) {
-  ev.waitUntil(
-    caches.open('servicew')
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll([
-          '/',
-          'css/style.css',
-          'js/main.js',
-          'js/restaurant_info.js',
-          '/index.html',
-          '/restaurant.html',
-          '/data/restaurants.json',
-          '/img/1.jpg',
-          '/img/2.jpg',
-          '/img/3.jpg',
-          '/img/4.jpg',
-          '/img/5.jpg',
-          '/img/6.jpg',
-          '/img/7.jpg',
-          '/img/8.jpg',
-          '/img/9.jpg',
-          '/img/10.jpg',
-          //'https://maps.googleapis.com/maps/api/js'
-        ]);
-      })
-  );
-});
-
-/* change: This js file has service worker codes.  */
-
 self.addEventListener('install', function(ev) {
   ev.waitUntil(
     caches.open('restaurant-service')
