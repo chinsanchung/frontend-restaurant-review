c6cc9874c41cf5b612a65d9586465014feaeae84
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2018-07-24 17:15:59 +0900

    Fix sw.js
    change some codes

--- sw.js
+++ sw.js
@@ -1,35 +1,34 @@
 /* change: This js file has service worker codes.  */
-self.addEventListener('install', function(ev) {
-  ev.waitUntil(
-    caches.open('restaurant-service')
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll([
-          'restaurant.html',
-          'index.html',
-          'js/main.js',
-          'js/restaurant_info.js',
-          'css/styles.css',
-          'img/1.jpg',
-          'img/2.jpg',
-          'img/3.jpg',
-          'img/4.jpg',
-          'img/5.jpg',
-          'img/6.jpg',
-          'img/7.jpg',
-          'img/8.jpg',
-          'img/9.jpg',
-          'img/10.jpg'
-        ]);
-      })
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('restaurant-app')
+    .then(function(cache) {
+      return cache.addAll([
+        'restaurant.html',
+        'index.html',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'css/styles.css',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response) return response;
-            return fetch(event.request);
-        })
-)});
-
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  )
+});
