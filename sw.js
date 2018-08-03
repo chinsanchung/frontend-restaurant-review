3842746035e762735379e88a64e35d3eea4e7691
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2018-08-03 14:53:55 +0900

    Fix sw.js
    Change function to arrow function and add comments

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-/* change: This js file has service worker codes.  */
-let serviceCache = 'restaurant-04'
-self.addEventListener('install', function(event) {
-
+//Install service worker and add files to chache storage.
+let serviceCache = 'restaurant-05'
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(serviceCache).then((cache) => {
       return cache.addAll([
         'restaurant.html',
@@ -22,36 +22,57 @@ self.addEventListener('install', function(event) {
         'img/10.jpg'
       ]).then(() => self.skipWaiting());
     })
-
+  )
 });
 
-//Make service worker to have fetch event
-self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
-  event.respondWith(
-    caches.open(serviceCache).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
+
+//Activate service worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName.startsWith('restaurant-') &&
+            cacheName != serviceCache;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
 
-//Activate service worker
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('restaurant-') &&
-            cacheName != serviceCache;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
+//Make service worker to start fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    /*Pass promise at caches.match(). This method checks
+    request and find resulf of cached that was made by Service worker*/
+    caches.match(event.request).then((response) => {
+      if(response) {
+        console.log('Found cache: ', event.request.url);
+        return response;
+      }
+
+      //Clone request for fetch.
+      let requestClone = event.request.clone();
+
+      return fetch(requestClone).then((response) => {
+        //Pass response to browser
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          console.log('No response : status is not 200 or type is not basic');
+          return response;
+        }
+        /*Clone response because browser needs to return response
+        and pass cache.*/
+        let responseToCache = response.clone();
+        return caches.open(serviceCache).then((cache) => {
+          //Pass response(clone) to cache
+          cache.put(event.request, responseToCache);
+          return response;
+        }).catch((error) => {
+          console.log('Service worker error')
         })
-      );
+      })
     })
   );
 });
